dbc10cb653679910c06e8ff90bb997c9f6ebac10
Tina54321 <167315319+Tina54321@users.noreply.github.com>
2024-05-08 09:35:59 +0000

    modify

--- lesson6/lesson6_3.py
+++ lesson6/lesson6_3.py
@@ -1,4 +1,7 @@
 import pyinputplus as pyip
 
-score = pyip.inputInt("請輸入學生分數",min=0,max=300)
-print(score)
+name = input('請輸入姓名:')
+height = pyip.inputFloat('請輸入身高(120~230)(cm):',min=120,max=230) 
+weight = pyip.inputFloat('請輸入體重(40~170)(kg):',min=40,max=170)
+bmi = weight / (height / 100) ** 2
+print(f'{name}, 您的BMI: {round(bmi,ndigits=2)}')
