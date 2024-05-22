697f1c7ffc86856aaa9a564b9ddd557e7ad4dbf5
Tina54321 <167315319+Tina54321@users.noreply.github.com>
2024-05-22 09:07:10 +0000

    m

---
+++ lesson10/lesson10_2.py
@@ -0,0 +1,13 @@
+import random
+import pyinputplus as pyip
+
+# (run .py檔時) names.txt此檔案存在lesson10,並分在根目錄,所以若檔案在該資料夾必須重新打開在該目錄的終端機
+with open('names.txt',encoding='utf-8') as file:  
+    content:str = file.read()
+
+n: int = pyip.inputInt("請輸入取得名字的數量", min=0, max=200)
+names:list[str] = content.split(sep='\n')
+random_names:list[str] = random.choices(population=names, k=n)
+for name in random_names:
+    print(name, end=' ')
+print()
