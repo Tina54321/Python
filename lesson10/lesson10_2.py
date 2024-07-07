18f8c6618a694b540fbe1965da22d9e0ac0c373c
Tina54321 <a6101714@gmail.com>
2024-07-07 17:05:11 +0800

    local

--- lesson10/lesson10_2.py
+++ lesson10/lesson10_2.py
@@ -6,7 +6,7 @@ with open('names.txt',encoding='utf-8') as file:
     content:str = file.read()
 
 n: int = pyip.inputInt("請輸入取得名字的數量", min=0, max=200)
-names:list[str] = content.split(sep='\n')
+names:list[str] = content.split(sep='\n') # 文字變串列-->可擷取需要範圍
 random_names:list[str] = random.choices(population=names, k=n)
 for name in random_names:
     print(name, end=' ')
